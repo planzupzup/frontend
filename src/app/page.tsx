3f79842ced1ad67cdc122fc93ce714eb5b450594
hwan12321 <kdh643737@gmail.com>
2025-05-19 13:00:26 +0900

    :lipstick: 하단 퍼블리싱

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -39,6 +39,62 @@ const Home: React.FC = () => {
           </li>
         </ul>
       </section>
+      <section className={styles.section_5}>
+        <h2 className={styles.title}>당신만의 여행을 만들어보세요!</h2>
+        <p className={styles.desc}>유튜버의 루트를 참고해, 내 여행에 맞게 플랜을 짜보세요<br />장소, 일정, 동선까지 한눈에 정리할 수 있어요</p>
+        <a href="#" className={styles.link}>플랜 만들기</a>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <img src="/img_jeju_island.png" alt="섬네일" className={styles.img}/>
+            <div className={styles.info_area}>
+              <div className={styles.day_wrap}>
+                <span className={styles.day}>N일차</span>
+              </div>
+              <h3 className={styles.sub_title}>[일이삼사오육칠팔구십]</h3>
+              <p className={styles.sub_desc}>일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십</p>
+            </div>
+          </li>
+          <li className={styles.item}>
+            <img src="/img_jeju_island.png" alt="섬네일" className={styles.img}/>
+            <div className={styles.info_area}>
+              <div className={styles.day_wrap}>
+                <span className={styles.day}>N일차</span>
+              </div>
+              <h3 className={styles.sub_title}>[일이삼사오육칠팔구십]</h3>
+              <p className={styles.sub_desc}>일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십</p>
+            </div>
+          </li>
+        </ul>
+      </section>
+      <section className={styles.section_6}>
+        <h2 className={styles.title}>함께 소통하고 공유해요</h2>
+        <p className={styles.desc}>옹심이들의 여행루트도 살펴보고 공유할 수 있어요!</p>
+        <div className={styles.link_wrap}>
+          <a href="#" className={styles.link}>플랜 만들기</a>
+        </div>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <img src="/img_jeju_island.png" alt="섬네일" className={styles.img}/>
+            <div className={styles.info_area}>
+              <div className={styles.day_wrap}>
+                <span className={styles.day}>N일차</span>
+              </div>
+              <h3 className={styles.sub_title}>[일이삼사오육칠팔구십]</h3>
+              <p className={styles.sub_desc}>일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십</p>
+            </div>
+          </li>
+          <li className={styles.item}>
+            <img src="/img_jeju_island.png" alt="섬네일" className={styles.img}/>
+            <div className={styles.info_area}>
+              <div className={styles.day_wrap}>
+                <span className={styles.day}>N일차</span>
+              </div>
+              <h3 className={styles.sub_title}>[일이삼사오육칠팔구십]</h3>
+              <p className={styles.sub_desc}>일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십일이삼사오육칠팔구십</p>
+            </div>
+          </li>
+        </ul>
+      </section>
     </>
   );
 };
