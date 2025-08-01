4a9b26c275a500d86a42c4be6a78c9c912059cb1
hwan12321 <kdh643737@gmail.com>
2025-08-01 15:29:50 +0900

    :lipstick: 메인 캐로셀 딤드 그라데이션 추가, 푸터 디자인가이드 적용

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,26 +3,10 @@ import style from "./Footer.module.scss";
 const Footer = () => {
     return (
         <footer className={style.footer_wrap}>
-            <ul className={style.list}>
-                <li className={style.item}>
-                    © 2025
-                </li>
-                <li className={style.item}>
-                    개인정보 처리방침
-                </li>
-                <li className={style.item}>
-                    쿠키 정책
-                </li>
-                <li className={style.item}>
-                    이용약관
-                </li>
-                <li className={style.item}>
-                    사이트맵
-                </li>
-            </ul>
-            <p className={style.text}>
-                웹사이트 제공자
-            </p>
+            <div className={style.email}>Plan zupzup 대표 홍길동<br />
+                Contact @Plan zupzup.co.kr
+            </div>
+            <div  className={style.right}>Copyright Plan zupzup. All Right Reserved.</div>
         </footer>
     )   
 }
