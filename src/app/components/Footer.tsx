174bdc60edbe8c1613cb9c578fceabca0f4b39f0
xorms571 <xorms6865@naver.com>
2025-11-12 11:47:35 +0900

    refactor: Update Footer component styles for improved layout and responsiveness; adjust padding and structure

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,12 +3,14 @@ import style from "./Footer.module.scss";
 const Footer = () => {
     return (
         <footer className={style.footer_wrap}>
-            <div className={style.email}>Plan zupzup 대표 김희영<br />
-                Contact huiyeong9619@naver.com
+            <div className={style.footer_inner}>
+                <div className={style.email}>Plan zupzup 대표 김희영<br />
+                    Contact huiyeong9619@naver.com
+                </div>
+                <div className={style.right}>Copyright Plan zupzup. All Right Reserved.</div>
             </div>
-            <div  className={style.right}>Copyright Plan zupzup. All Right Reserved.</div>
         </footer>
-    )   
+    )
 }
 /* eslint-disable */
 
